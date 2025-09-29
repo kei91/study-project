e20b817a88441620b0397f55f053e9c3b5197554
Ekaterina Bkhandari <katerinabk9@gmail.com>
2025-09-29 22:45:16 +0100

    shared mutex on seqlock

---
+++ week3-2209/seqlock.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <thread>
+#include <syncstream>
+#include <vector>
+
+// seqlock
+int main()
+{
+	int epoch = 0;
+	long long sum = 0;
+
+	auto reader = [&](const int id) {
+
+		int before = epoch;
+		long long tempSum = sum;
+		int after = epoch;
+
+		while (after % 2 == 1 || before % 2 == 1 || before != after) {
+			before = epoch;
+			tempSum = sum;
+			after = epoch;
+		}
+
+		std::osyncstream(std::cout) << "sum: " << tempSum << '\n';
+	};
+
+	auto writer = [&]() {
+		for (int i = 0; i < 10000; ++i) {
+			++epoch;
+			++sum;
+			++epoch;
+		}
+	};
+
+	std::vector<std::thread> threads_read;
+	std::thread threads_write(writer);
+
+	for (int i = 0; i < 100; ++i) {
+		threads_read.emplace_back(reader, i);
+	}
+
+	for (int i = 0; i < 100; ++i) {
+		threads_read[i].join();
+	}
+
+	threads_write.join();
+
+	std::cout << sum << std::endl;
+}
