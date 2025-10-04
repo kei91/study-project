12ecc085a8842dd7bb0a4752b5d10624f03e7afa
Ekaterina Bkhandari <katerinabk9@gmail.com>
2025-10-04 14:58:20 +0100

    week4 tasks 1/2

---
+++ week4-0210/GetSetSetAll.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <vector>
+
+//GetSetSetAll
+constexpr int N = 8;
+
+int main()
+{
+	int epoch = 0;
+	std::vector<std::pair<int, int>> data(N);
+	std::fill(data.begin(), data.end(), std::pair<int, int>(epoch,0));
+
+	std::pair<int, int> setAllData(epoch, 0);
+
+	auto get = [&](int index) -> int {
+		if (setAllData.first > data[index].first)
+			return setAllData.second;
+
+		return data[index].second;
+	};
+
+	auto set = [&](int index, int value) {
+		++epoch;
+		data[index].first = epoch;
+		data[index].second = value;
+	};
+
+	auto setAll = [&](int value) {
+		++epoch;
+		setAllData.first = epoch;
+		setAllData.second = value;
+	};
+
+	std::cout << get(0) << std::endl;
+
+	set(0, 1);
+	std::cout << get(0) << std::endl;
+
+	setAll(2);
+	std::cout << get(0) << std::endl;
+
+	set(0, 3);
+	std::cout << get(0) << std::endl;
+}
---
+++ week4-0210/spsc.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <thread>
+#include <syncstream>
+#include <vector>
+
+//SPSC Lock-free bounded queue
+
+constexpr int N = 8;
+
+int main()
+{
+	std::vector<std::atomic<int>> data(N);
+	std::fill(data.begin(), data.end(), -1);
+
+	int write = 0;
+	int read = 0;
+
+	auto push = [&](int value) {
+		while (data[write].load(std::memory_order_acquire) != -1) {
+			std::this_thread::yield();
+		}
+
+		data[write].store(value, std::memory_order_release);
+
+		write = (write + 1) & (N - 1);
+	};
+
+	auto pop = [&]() {
+		while (data[read].load(std::memory_order_acquire) == -1) {
+			std::this_thread::yield();
+		}
+
+		data[read].store(-1, std::memory_order_release);
+
+		read = (read + 1) & (N - 1);
+	};
+
+	std::thread thread_read(pop);
+	std::thread thread_write(push, 50);
+	thread_write.join();
+	thread_read.join();
+}
